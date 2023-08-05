0b537b61a4f65e2a51e14f6ac043afce17a41d65
Paramesh-R <parameswaran.r@outlook.com>
2023-08-06 00:01:11 +0530

    updated Homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import MainPage from './pages/MainPage';
-// import Homepage from './pages/Homepage';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-      {/* <Homepage /> */}
-      <MainPage/>
+      <Homepage />
+      
     </div>
   );
 }
